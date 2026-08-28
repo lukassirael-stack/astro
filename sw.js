2331722682760dd86e5785bff7e17f85647a1253
lukassirael-stack <lukas.sirael@gmail.com>
2026-08-28 10:03:16 +0200

    Hlavicka: navrat souhvezdi Plejad (pruhledny vyrez z loga) mezi nazev a claim + SW v103

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Kairos – service worker. Při každé změně index.html zvedni číslo verze. */
-const CACHE = 'kairos-v102';
-const SHELL = ['./', './index.html', './astronomy.browser.min.js', './manifest.webmanifest', './logo.png', './logo-emblem.webp?v=1', './splash.webp?v=1', './moon-tex.png?v=1', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './sky-day.webp?v=7', './sky-night.webp?v=4', './sky-night-hd.webp?v=1', './sky-day-hd.webp?v=1'];
+const CACHE = 'kairos-v103';
+const SHELL = ['./', './index.html', './astronomy.browser.min.js', './manifest.webmanifest', './logo.png', './logo-emblem.webp?v=1', './logo-stars.webp?v=1', './splash.webp?v=1', './moon-tex.png?v=1', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './sky-day.webp?v=7', './sky-night.webp?v=4', './sky-night-hd.webp?v=1', './sky-day-hd.webp?v=1'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL.map((u) => new Request(u, { cache: 'reload' })))).then(() => self.skipWaiting()));
